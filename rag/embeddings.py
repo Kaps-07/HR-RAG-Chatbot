2531b979f893f41cec93f5d0c55192f69954966a
Kapil Patil <devscar7@gmail.com>
2026-08-30 15:55:24 +0530

    Add project configuration

--- rag/embeddings.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import AzureOpenAIEmbeddings
-from config import AZURE_OPENAI_EMBEDDING_MODEL, AZURE_OPENAI_ENDPOINT, AZURE_OPENAI_KEY, AZURE_OPENAI_API_VERSION
-
-embeddings = AzureOpenAIEmbeddings(
-    azure_deployment=AZURE_OPENAI_EMBEDDING_MODEL,
-    azure_endpoint=AZURE_OPENAI_ENDPOINT,
-    api_key=AZURE_OPENAI_KEY,
-    api_version=AZURE_OPENAI_API_VERSION,
-)
